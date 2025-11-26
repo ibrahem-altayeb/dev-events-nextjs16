a203dcee8f8cf7ce91b170b54a68478107acefe3
ibrahem-altayeb <Brh.altayeb@gmail.com>
2025-11-26 05:33:27 +0300

    first commit

---
+++ lib/constants.tsx
@@ -0,0 +1,57 @@
+export type EventItme = {
+    image: string,
+    title: string   ,
+    slug: string,
+    location: string,
+    date: string,
+    time: string,
+}
+
+
+export const events:EventItme[] = [
+    {
+        image : "/images/event1.png",
+        title : "React summit",
+        slug : "React summit-us 2025",
+        location : "USA",
+        date : "2025-11-7" ,
+        time : "09:00 pm" ,
+    } , {
+        image : "/images/event2.png",
+        title : "React summit",
+        slug : "React summit-us 2025",
+        location : "USA",
+        date : "2025-11-7" ,
+        time : "09:00 pm" ,
+    } , {
+        image : "/images/event3.png",
+        title : "React summit",
+        slug : "React summit-us 2025",
+        location : "USA",
+        date : "2025-11-7" ,
+        time : "09:00 pm" ,
+    } , {
+        image : "/images/event4.png",
+        title : "React summit",
+        slug : "React summit-us 2025",
+        location : "USA",
+        date : "2025-11-7" ,
+        time : "09:00 pm" ,
+    } , {
+        image : "/images/event5.png",
+        title : "React summit",
+        slug : "React summit-us 2025",
+        location : "USA",
+        date : "2025-11-7" ,
+        time : "09:00 pm" ,
+    } ,
+    {
+        image : "/images/event6.png",
+        title : "React summit",
+        slug : "React summit-us 2025",
+        location : "USA",
+        date : "2025-11-7" ,
+        time : "09:00 pm" ,
+    } ,
+
+]
